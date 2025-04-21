53f26818b226dad9d28f9578f2e4d65993f77a10
Yee334 <2418748180@qq.com>
2025-04-21 20:21:52 +0800

    cleaned up embedded repos and initial commit

--- scripts/finetune_joint.sh
+++ scripts/finetune_joint.sh
@@ -10,31 +10,56 @@
 # 8th arg: config name
 # 9th arg: pretrained model path
 
-ARRAY=()
+# ARRAY=()
 
+# for i in "$@"
+# do 
+#     echo $i
+#     ARRAY+=("${i}")
+# done
+
+
+# python src/retrieve.py --target_name "${ARRAY[0]}" --outpath ${ARRAY[2]}
+# python src/retrieve.py --target_name "${ARRAY[3]}" --outpath ${ARRAY[5]}
+
+
+# python -u  train.py \
+#         --base configs/custom-diffusion/${ARRAY[7]}  \
+#         -t --gpus 1 \
+#         --resume-from-checkpoint-custom  ${ARRAY[8]} \
+#         --caption "<new1> ${ARRAY[0]}" \
+#         --datapath ${ARRAY[1]} \
+#         --reg_datapath "${ARRAY[2]}/images.txt" \
+#         --reg_caption "${ARRAY[2]}/caption.txt" \
+#         --caption2 "<new2> ${ARRAY[3]}" \
+#         --datapath2 ${ARRAY[4]} \
+#         --reg_datapath2 "${ARRAY[5]}/images.txt" \
+#         --reg_caption2 "${ARRAY[5]}/caption.txt" \
+#         --modifier_token "<new1>+<new2>" \
+#         --name "${ARRAY[6]}-sdv4"
+
+
+ARRAY=()
+ 
 for i in "$@"
-do 
+do
     echo $i
     ARRAY+=("${i}")
 done
-
-
-python src/retrieve.py --target_name "${ARRAY[0]}" --outpath ${ARRAY[2]}
-python src/retrieve.py --target_name "${ARRAY[3]}" --outpath ${ARRAY[5]}
-
-
+ 
+ 
 python -u  train.py \
         --base configs/custom-diffusion/${ARRAY[7]}  \
-        -t --gpus 0,1 \
+        -t --gpus 1 \
         --resume-from-checkpoint-custom  ${ARRAY[8]} \
         --caption "<new1> ${ARRAY[0]}" \
         --datapath ${ARRAY[1]} \
-        --reg_datapath "${ARRAY[2]}/images.txt" \
-        --reg_caption "${ARRAY[2]}/caption.txt" \
+        --reg_datapath "${ARRAY[2]}/samples" \
+        --reg_caption "${ARRAY[0]}" \
         --caption2 "<new2> ${ARRAY[3]}" \
         --datapath2 ${ARRAY[4]} \
-        --reg_datapath2 "${ARRAY[5]}/images.txt" \
-        --reg_caption2 "${ARRAY[5]}/caption.txt" \
+        --reg_datapath2 "${ARRAY[5]}/samples" \
+        --reg_caption2 "${ARRAY[3]}" \
         --modifier_token "<new1>+<new2>" \
         --name "${ARRAY[6]}-sdv4"
 
